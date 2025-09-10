5b2e5bd45a68bf5a20ba22ce805520bc7f62d7ba
Arhon Pineda <apineda@wsfcu.com>
2025-09-10 15:06:00 -0500

    remove repository files

--- api/Controllers/ItemController.cs
+++ api/Controllers/ItemController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
-using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,17 +14,15 @@ namespace api.Controllers
     public class ItemController : ControllerBase
     {
         private readonly InventoryDbContext _context;
-        private readonly IItemRepository _itemRepo;
-        public ItemController(InventoryDbContext context, IItemRepository itemRepo)
+        public ItemController(InventoryDbContext context)
         {
             _context = context;
-            _itemRepo = itemRepo;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var items = await _itemRepo.GetAllAsync();
+            var items = await _context.Items.ToListAsync();
             return Ok(items);
         }
 
