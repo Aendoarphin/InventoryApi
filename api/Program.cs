6b948a281d19254ab6746fd4e901ddeec2c20671
Arhon Pineda <apineda@wsfcu.com>
2025-10-17 17:15:17 -0500

    add public index page

--- api/Program.cs
+++ api/Program.cs
@@ -49,7 +49,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(AllowReactClient);
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.MapControllers();
-app.MapGet("/", () => "INVENTORY API");
+app.MapGet("/", () => Results.Redirect("/index.html"));
 
 app.Run();
