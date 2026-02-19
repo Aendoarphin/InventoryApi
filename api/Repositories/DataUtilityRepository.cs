c56b7773889b49bee5a06997a2eabcd0377fab8f
Arhon Pineda <arhon.k.pineda@gmail.com>
2026-02-18 21:36:19 -0600

    add more members

--- api/Repositories/DataUtilityRepository.cs
+++ api/Repositories/DataUtilityRepository.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.Data;
+using Api.Dtos;
 using Api.Interfaces;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Api.Repositories
 {
@@ -33,7 +35,81 @@ namespace Api.Repositories
             var entityCount = await _context.Set<T>().CountAsync();
             return entityCount;
         }
-    }
-}
 
-// continue here
+        public async Task<List<T>> GetPartial<T>() where T : class
+        {
+            var lambda = BuildNullCheckExpression<T>();
+            if (lambda == null) return new List<T>();
+            return await _context.Set<T>().Where(lambda).ToListAsync();
+
+        }
+
+        public async Task<int> GetPartialCount<T>() where T : class
+        {
+            var lambda = BuildNullCheckExpression<T>();
+            if (lambda == null) return 0;
+            return await _context.Set<T>().CountAsync(lambda);
+        }
+
+        public async Task<RecordMetricDto> GetMetrics<T>() where T : class
+        {
+            var partialCount = await GetPartialCount<T>();
+            var totalCount = await GetCount<T>();
+            return new RecordMetricDto
+            {
+                Complete = totalCount - partialCount,
+                Partial = partialCount
+            };
+        }
+
+        public async Task<T?> Create<T>(T entity) where T : class
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T?> Update<T>(T entity) where T : class
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T?> Delete<T>(int id) where T : class
+        {
+            var record = await _context.FindAsync<T>(id);
+            if (record == null) return null;
+            _context.Set<T>().Remove(record);
+            await _context.SaveChangesAsync();
+            return record;
+        }
+
+        /// <summary>
+        /// Builds a lambda expression like: x => x.Prop1 == null || x.Prop2 == null ...
+        /// for all nullable properties on T.
+        /// </summary>
+        private static Expression<Func<T, bool>>? BuildNullCheckExpression<T>() where T : class
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            Expression? body = null;
+
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                // Check for reference type or Nullable<T>
+                if (!prop.PropertyType.IsValueType || Nullable.GetUnderlyingType(prop.PropertyType) != null)
+                {
+                    var propAccess = Expression.Property(parameter, prop);
+                    var nullConstant = Expression.Constant(null, prop.PropertyType);
+                    var equals = Expression.Equal(propAccess, nullConstant);
+
+                    body = body == null ? equals : Expression.OrElse(body, equals);
+                }
+            }
+
+            if (body == null) return null;
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+    }
+} // continue here
